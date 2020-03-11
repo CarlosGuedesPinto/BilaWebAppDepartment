0103e8f74bf67879f2fea276c17e1f7b7750b400
unknown <bernardo.caiano@outlook.pt>
2020-03-11 17:20:08 +0000

    home page

--- FriconV1/screens/home.js
+++ FriconV1/screens/home.js
@@ -5,14 +5,9 @@ import {MaterialIcons} from '@expo/vector-icons'
 export default function Home() {
 
     const dimensions = Dimensions.get('window');
-    const imageHeight =dimensions.height / 2
     const imageWidth = dimensions.width;
-   
-    console.log (imageHeight)
-    console.log(imageWidth)
-   
+ 
     return (
-       
         <View style={styles.container}>
             <StatusBar backgroundColor="white" barStyle="dark-content" />
             <View style={styles.header}>
@@ -22,18 +17,12 @@ export default function Home() {
             <Image source={require('../assets/CatálogoProduto.png')} style={{flex:1, width: imageWidth }} />
             <Image source={require('../assets/PontosVenda.png')} style={{flex:1, width: imageWidth }} />
         </View>
-      
-       
-       
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-     
-       
-      
     },
 
     header: {
@@ -44,8 +33,5 @@ const styles = StyleSheet.create({
         paddingRight: 20,
         paddingTop: 45,
         backgroundColor: 'white'
-        
-      
     },
-    
 })
