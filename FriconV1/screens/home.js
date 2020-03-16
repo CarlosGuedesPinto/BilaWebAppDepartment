3dd993b2c7bd81aa616f4b36fbb363d1ef03e000
Carlos Pinto Guedes <carlosguedespinto@gmail.com>
2020-03-16 11:16:46 +0000

    Chanes
    Changes

--- FriconV1/screens/home.js
+++ FriconV1/screens/home.js
@@ -1,21 +1,21 @@
 import React from "react";
-import {StyleSheet, View, Image, Text, Dimensions, ScrollView, StatusBar} from "react-native";
-import {MaterialIcons} from '@expo/vector-icons'
+import { StyleSheet, View, Image, Text, Dimensions, ScrollView, StatusBar, TouchableHighlight } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons'
 
 export default function Home() {
 
     const dimensions = Dimensions.get('window');
     const imageWidth = dimensions.width;
- 
+
     return (
         <View style={styles.container}>
             <StatusBar backgroundColor="white" barStyle="dark-content" />
             <View style={styles.header}>
-                <Image source={require('../assets/Logo.png')} style={{width: 150, height: 44}} />
-                <MaterialIcons name="menu" size={40} color={"#002169"}/>
-            </View> 
-            <Image source={require('../assets/CatálogoProduto.png')} style={{flex:1, width: imageWidth }} />
-            <Image source={require('../assets/PontosVenda.png')} style={{flex:1, width: imageWidth }} />
+                <Image source={require('../assets/Logo.png')} style={{ width: 150, height: 44 }} />
+                <MaterialIcons name="menu" size={40} color={"#002169"} />
+            </View>
+            <Image source={require('../assets/CatalogoProduto.png')} style={{ flex: 1, width: imageWidth }} />
+            <Image source={require('../assets/PontosVenda.png')} style={{ flex: 1, width: imageWidth }} />
         </View>
     )
 }
